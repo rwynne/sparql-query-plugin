05c30d59acbdb18145a8683440b2b253789426b9
yinghua Xu <yinghua_1@yahoo.com>
2018-05-14 22:34:19 -0400

    Added Export Button functionality and refresh Bookmark Panel after saving a new bookmark.

--- src/main/java/org/protege/editor/owl/rdf/SparqlQueryView.java
+++ src/main/java/org/protege/editor/owl/rdf/SparqlQueryView.java
@@ -19,31 +19,30 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class SparqlQueryView extends AbstractOWLViewComponent {
 	private static final long serialVersionUID = -1370725700740073290L;
 	
+	protected static final String LAST_EXPORT_FOLDER = "";
+	
 	private SparqlReasoner reasoner;
 	private JList bookmarkList;
-	//private BookmarkListModel bookmarkListModel;
 	private JTextPane queryPane;
 	private JButton executeQuery;
 	private JButton bookmarkBtn;
+	private JButton exportBtn;
 	private SwingResultModel resultModel;
 	
 	private Map<String, String> queryMap;
-	private Preferences prefs;
-
+	
 	@Override
 	protected void initialiseOWLView() throws Exception {
-		prefs = PreferencesManager.getInstance().getApplicationPreferences(getClass());  
-		
 		initializeReasoner();
 		initializeQueryMap();
 		setLayout(new BorderLayout());
@@ -65,7 +64,36 @@ public class SparqlQueryView extends AbstractOWLViewComponent {
 	private void initializeQueryMap() {
 		
 		queryMap = new HashMap<String, String>();
-		queryMap.put("bookmark test 1", reasoner.getSampleQuery());
+		
+		String filename = "." + File.separator + "bookmark";
+		File file = new File(filename);
+		String[] filenames = file.list();
+		
+		try {
+			for(int i = 0; i < filenames.length; i++) {
+				File localfile = new File("." + File.separator + "bookmark" + File.separator + filenames[i]);
+				Scanner scanner = new Scanner(localfile);
+				
+				String value = scanner.useDelimiter("\\Z").next();
+				
+				String bookmark = null; 
+				if(!filenames[i].contains(".")) {
+					bookmark = filenames[i];
+				}
+				else {
+					bookmark = filenames[i].substring(0, filenames[i].indexOf('.'));
+				}
+				
+				queryMap.put(bookmark, value);
+		
+				scanner.close();
+			}
+		}
+		catch(Exception ex) {
+			ErrorLogPanel.showErrorDialog(ex);
+			JOptionPane.showMessageDialog(getOWLWorkspace(), ex.getMessage() + "\nSee the logs for more information.");			
+		}
+		/* queryMap.put("bookmark test 1", reasoner.getSampleQuery());
 		queryMap.put("bookmark test 2", "prefix ncit: <http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#>\n" + 
 				"prefix owl:  <http://www.w3.org/2002/07/owl#>\n" + 
 				"prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + 
@@ -76,16 +104,22 @@ public class SparqlQueryView extends AbstractOWLViewComponent {
 				"	optional {?s rdfs:label ?l .}\n" + 
 				"	minus { ?s ncit:Semantic_Type ?x. }\n" + 
 				"	}\n" + 
-				"LIMIT 15");
+				"LIMIT 15"); */
 	}
 	private JComponent createCenterComponent() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(0,1));
 		DefaultListModel<String> model = new DefaultListModel<>();
 		Set<String> bookmarkKeys = queryMap.keySet();
+		//first add new query to list and newquery key pair to hasmap
+		model.addElement("NewQuery");		
+		
 		for(String bookmark : bookmarkKeys) {
 			model.addElement(bookmark);
 		}
+		
+		queryMap.put("NewQuery", "");
+		
 		//model.addElement("bookmark test 1");
 		//model.addElement("bookmark test 2");
 		bookmarkList = new JList(model);
@@ -133,7 +167,7 @@ public class SparqlQueryView extends AbstractOWLViewComponent {
 	
 	private JComponent createBottomComponent() {
 		JPanel panel = new JPanel();
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 10));
 		executeQuery = new JButton("Execute");
 		
 		executeQuery.addActionListener(new ActionListener() {
@@ -181,7 +215,60 @@ public class SparqlQueryView extends AbstractOWLViewComponent {
 				}
 			}
 		});
+		
 		panel.add(bookmarkBtn);
+		
+		exportBtn = new JButton("Export");
+		exportBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Preferences prefs = PreferencesManager.getInstance().getApplicationPreferences(getClass());   
+					JFileChooser fc = new JFileChooser(prefs.getString(LAST_EXPORT_FOLDER, new File(".").getAbsolutePath()));
+					 // if(inputFolder != null){
+					//	  fc.setCurrentDirectory(inputFolder);
+					//  }
+					  //todo - add file extension filter
+					  int select = fc.showSaveDialog(null);
+
+					if (select == JFileChooser.APPROVE_OPTION) {
+						 File file = fc.getSelectedFile();
+						 prefs.putString(LAST_EXPORT_FOLDER, file.getParent());
+						  //String outfile = file.getAbsolutePath();
+						 FileWriter writer = new FileWriter(file);
+							//System.out.println(query);
+						 for(int i = 0; i < resultModel.getRowCount(); i++) {
+							 int colcount = resultModel.getColumnCount();
+							 StringBuilder builder = new StringBuilder();
+							 for(int j = 0; j < colcount; j++) {
+								 builder.append(resultModel.getValueAt(i, j).toString());
+								 if(j < (colcount -1)) {
+									 builder.append('\t');
+								 }
+							 }
+							 
+							 
+							  writer.write( builder.toString() + '\n');
+						 }
+							
+						writer.flush();
+						writer.close();
+						  
+					 }
+					
+				}
+				catch (Exception ex) {
+					ErrorLogPanel.showErrorDialog(ex);
+					JOptionPane.showMessageDialog(getOWLWorkspace(), ex.getMessage() + "\nSee the logs for more information.");
+				}
+			}
+		});
+		
+		
+		panel.add(exportBtn);
+		
+		
 		return panel;
 	}
 
@@ -196,6 +283,14 @@ public class SparqlQueryView extends AbstractOWLViewComponent {
 		writer.write(query);
 		writer.flush();
 	    writer.close();
+	    //update queryMap
+		queryMap.put(bookmark, query);
+		//set bookmark as selected
+		
+		DefaultListModel<String> model = (DefaultListModel)bookmarkList.getModel();
+		model.addElement(bookmark);
+		bookmarkList.setSelectedIndex(model.getSize() - 1);
+		
 	}
 	
 	@Override
